a75e358bb6baaefc50dc2471a6a0c3cbbb31e268
mattstrathearn <matthew.strathearn@gmail.com>
2020-09-26 14:20:31 -0400

    raw mode terminal

--- text_editor_c.c
+++ text_editor_c.c
@@ -4,10 +4,38 @@
 #include <ctype.h>
 #include <stdio.h>
 
+struct termios original_settings;
+
+void enableRawMode(){
+
+	tcgetattr(STDIN_FILENO, &original_settings);
+
+	struct termios raw = original_settings;
+
+	raw.c_lflag &= ~(ECHO | ICANON);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+void disableRawMode(){
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_settings);
+
+}
+
 int main(){
+
+	atexit(disableRawMode);
+	
+	enableRawMode();
 
 	char c;
 
-	while(read(STDIN_FILENO, &c, 1) == 1 && c!='q');
+	while(read(STDIN_FILENO, &c, 1) == 1 && c!='q'){
+
+		printf("%d\n", c);
+
+	}
 
 }
